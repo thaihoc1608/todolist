ced8ed65dc9c3f08979b3a40defdbe3750f04f77
thaihoc1608 <127484396+thaihoc1608@users.noreply.github.com>
2024-07-30 21:17:08 +0700

    Add files via upload

---
+++ todolist_official/Todolist_Official.java
@@ -0,0 +1,12 @@
+package todolist_official;
+
+public class Todolist_Official {
+
+    public static void main(String[] args) {
+            
+        LoginForm lgf = new LoginForm();
+        lgf.setVisible(true);
+        lgf.setLocationRelativeTo(null);
+    }
+    
+}
